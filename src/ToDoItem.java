0b1f2eea01ad5f4b5586e37d18a8368b5e9b4884
Leonixu <107299451+Leonixu@users.noreply.github.com>
2024-03-06 10:08:33 +1000

    added some stuff

---
+++ src/ToDoItem.java
@@ -0,0 +1,26 @@
+public class ToDoItem {
+    private String description;
+    private boolean isDone;
+
+    // TODO Now:
+    // Add a constructor to initialize the item with the description, and isDone as false, with a single parameter for the description
+    // Add getters and setters for each field
+    public ToDoItem(String description) {
+        this.description = description;
+        isDone = false;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    public boolean getIsDone() {
+        return isDone;
+    }
+    public void setIsDone(boolean isDone) {
+        this.isDone = isDone;
+    }
+}
